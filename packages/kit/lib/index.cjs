d354dd9f88466a35e63e668b287efcb19f687783
Pooya Parsa <pyapar@gmail.com>
2021-11-18 11:28:18 +0100

    fix(kit): remove jiti based cjs proxy

--- packages/kit/lib/index.cjs
+++
@@ -1,3 +0,0 @@
-console.warn('[warning] You are using CommonJS version of `@nuxt/kit` that is an ESM-only package. CJS support will be dropped in stable release.')
-
-module.exports = require('jiti')(__dirname)('./index.mjs')
